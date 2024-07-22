032ff0a528403e222bbd42220bd0c75c62f7cc5a
João Paulo <costajoaopaulo113@gmail.com>
2024-07-22 09:16:49 -0300

    add: logica

--- src/app/aluno/aluno.component.ts
+++ src/app/aluno/aluno.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { IAluno } from '../alunos';
 
+interface Aluno {
+  nome: string;
+  primeiraNota: number;
+  segundaNota: number;
+}
+
 @Component({
   selector: 'app-aluno',
   templateUrl: './aluno.component.html',
@@ -8,20 +14,40 @@ import { IAluno } from '../alunos';
 })
 export class AlunoComponent implements OnInit {
 
-  alunos: IAluno[] = [];
+  alunos: Aluno[] = [];
+  novoAluno: Aluno = {nome: "", primeiraNota: 0, segundaNota: 0};
+  editIndex: number | null = null;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  calcularMedia(nota_1: number, nota_2: number) {
+  adicionarAluno() {
+    if(this.editIndex !== null){
+      this.alunos[this.editIndex] = {...this.novoAluno};
+      this.editIndex = null;
+    } else {
+      this.alunos.push({...this.novoAluno });
+    }
+    this.novoAluno = {nome: "", primeiraNota: 0, segundaNota: 0};
+  }
 
-    const soma = nota_1 + nota_2;
+  editarAluno(indice: number) {
+    this.novoAluno = {...this.alunos[indice]};
+    this.editIndex = indice;
+  }
 
-    const result = soma / 2;
-    return result;
+  excluirAluno(indice: number) {
+    this.alunos.splice(indice, 1);
+  }
 
+  calcularMedia(aluno: Aluno): number{
+    return (aluno.primeiraNota + aluno.segundaNota) / 2;
+  }
+
+  calcularCondicao(aluno: Aluno): string {
+    return this.calcularMedia(aluno) >= 6 ? "Aprovado" : "Reprovado";
   }
 
 }
--- src/app/alunos.ts
+++ src/app/alunos.ts
@@ -1,5 +1,5 @@
 export interface IAluno{
-  nota_1: number;
-  nota_2: number;
-  condicao: boolean;
+  nome: string;
+  primeiraNota: number;
+  segundaNota: number;
 }
